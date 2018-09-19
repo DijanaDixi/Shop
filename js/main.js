fb7f4bc1b99749c6c345b71263ce86cd878f2592
DijanaDixi <dijana.1986@live.com>
2018-09-19 16:12:05 +0200

    templejti

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-
+var template = $("#template").html();
 $(".back-to-top").click(function () {
-    $("html,body").animate({scrollTop: 0}, 1000);
+    $("html,body").animate({
+        scrollTop: 0
+    }, 1000);
 });
 
-$.ajax({
-    url:"https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
-    method:"get",
-    dataType:"json"
-})
-.done(function(res){
-    console.log(res)
-}
-)
+$
+    .ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
+    .done(function (res) {
+        console.log(res);
+        var text = "";
+        res.forEach(function (e) {
+            // console.log(e); svaki proizvod
+            text = template
+                .replace("{{imgSrc}}", e.imgSrc)
+                .replace("{{title}}", e.productTitle)
+                .replace("{{model}}", e.model)
+                .replace("{{price}}", e.price);
+            $("#mainRow").append(text)
+
+        })
+    })
