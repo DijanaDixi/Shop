21a0af209bbf1234f464e263b11b973b3bc10437
DijanaDixi <dijana.1986@live.com>
2018-09-20 22:01:45 +0200

    filtriranje2

--- js/main.js
+++ js/main.js
@@ -1,62 +1,68 @@
 var template = $("#template").html();
-
 var title = new RegExp('{{title}}', 'g')
 var mainRow = $("#mainRow")
-var collections = $("[data-collection]");
-// console.log(collections.length)
-
+var collections = $("[data-collection]"); //kategorije // console.log(collections.length)
 collections.on("click", displayCollections);
-
 display();
-
 $(".back-to-top").click(function () {
     $("html,body").animate({
         scrollTop: 0
     }, 1000);
 });
-
 function display() {
     mainRow.html("");
-    $.ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
+    $
+        .ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
         .done(function (res) {
-            console.log(res);
-            var text = "";
-            res.forEach(function (e) {
-                // console.log(e); svaki proizvod
-                text = template
-                    .replace("{{imgSrc}}", e.imgSrc)
-                    .replace(title, e.productTitle)
-                    .replace("{{model}}", e.model)
-                    .replace("{{price}}", e.price);
-                mainRow.append(text)
-
-            })
+            // console.log(res);             var text = "";
+            res
+                .forEach(function (e) { // console.log(e); svaki proizvod
+                    text = template
+                        .replace("{{imgSrc}}", e.imgSrc)
+                        .replace(title, e.productTitle)
+                        .replace("{{model}}", e.model)
+                        .replace("{{price}}", e.price);
+                    mainRow.append(text)
+                })
         })
 }
-
 function displayCollections(e) {
-    mainRow.html(""); //prvo isprazniti mainRow
+    mainRow.html(""); //prvo isprazniti
     e.preventDefault();
     var col = $(this).data("collection");
-   
 
+    $
+        .ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
+        .done(function (res) {
 
-$.ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
-    .done(function (res) {
-        var colFilter = res.filter(function (el) {
-            return el.colection === col;
+            if (col === "male" || col === "female") {
+                var colFilter = res.filter(function (el) {
+                    return el.colection === col;
+
+                })
+                var text = "";
+                colFilter.forEach(function (e) {
+                    text = template
+                        .replace("{{imgSrc}}", e.imgSrc)
+                        .replace(title, e.productTitle)
+                        .replace("{{model}}", e.model)
+                        .replace("{{price}}", e.price);
+                    mainRow.append(text)
+                })
+            } else if (col === "action" || col === "popular" || col === "newCol") {
+                var colFilter = res.filter(function (el) {
+                    return el[col];
+                })
+                var text = "";
+                colFilter.forEach(function (e) {
+                    text = template
+                        .replace("{{imgSrc}}", e.imgSrc)
+                        .replace(title, e.productTitle)
+                        .replace("{{model}}", e.model)
+                        .replace("{{price}}", e.price);
+                    mainRow.append(text)
+                })
+            }
+
         })
-        // console.log(colFilter)
-        var text = "";
-        colFilter.forEach(function (e) {
-           
-            text = template
-                .replace("{{imgSrc}}", e.imgSrc)
-                .replace(title, e.productTitle)
-                .replace("{{model}}", e.model)
-                .replace("{{price}}", e.price);
-            mainRow.append(text)
-        
-        })
-    })
 }
