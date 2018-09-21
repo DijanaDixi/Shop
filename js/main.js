ad6c231cf0eef84e608c37ce1b1d290ab4269583
DijanaDixi <dijana.1986@live.com>
2018-09-21 13:36:51 +0200

    refaktoring

--- js/main.js
+++ js/main.js
@@ -2,67 +2,60 @@ var template = $("#template").html();
 var title = new RegExp('{{title}}', 'g')
 var mainRow = $("#mainRow")
 var collections = $("[data-collection]"); //kategorije // console.log(collections.length)
-collections.on("click", displayCollections);
-display();
+
 $(".back-to-top").click(function () {
     $("html,body").animate({
         scrollTop: 0
     }, 1000);
 });
-function display() {
+
+window.onload = function () {
+    collections.parent().removeClass("active");
+    $.ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+            method: "get", 
+            dataType: "json"})
+    .done(function (res) {
+            displayCollections(res)
+            collections.on("click", function(){
+                displayCollections.call(this ,res)
+            });
+        })
+};
+
+function displayCollections(res) {
     mainRow.html("");
-    $
-        .ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
-        .done(function (res) {
-            // console.log(res);             var text = "";
-            res
-                .forEach(function (e) { // console.log(e); svaki proizvod
-                    text = template
-                        .replace("{{imgSrc}}", e.imgSrc)
-                        .replace(title, e.productTitle)
-                        .replace("{{model}}", e.model)
-                        .replace("{{price}}", e.price);
-                    mainRow.append(text)
-                })
-        })
-}
-function displayCollections(e) {
-    mainRow.html(""); //prvo isprazniti
-    e.preventDefault();
+    event.preventDefault();
     var col = $(this).data("collection");
+    console.log(this)
 
-    $
-        .ajax({url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json", method: "get", dataType: "json"})
-        .done(function (res) {
+    if (col === "male" || col === "female") {
+        var colFilter = res.filter(function (el) {
+            return el.colection === col;
+          })
+        displayProduct(colFilter);
 
-            if (col === "male" || col === "female") {
-                var colFilter = res.filter(function (el) {
-                    return el.colection === col;
-
-                })
-                var text = "";
-                colFilter.forEach(function (e) {
-                    text = template
-                        .replace("{{imgSrc}}", e.imgSrc)
-                        .replace(title, e.productTitle)
-                        .replace("{{model}}", e.model)
-                        .replace("{{price}}", e.price);
-                    mainRow.append(text)
-                })
-            } else if (col === "action" || col === "popular" || col === "newCol") {
-                var colFilter = res.filter(function (el) {
-                    return el[col];
-                })
-                var text = "";
-                colFilter.forEach(function (e) {
-                    text = template
-                        .replace("{{imgSrc}}", e.imgSrc)
-                        .replace(title, e.productTitle)
-                        .replace("{{model}}", e.model)
-                        .replace("{{price}}", e.price);
-                    mainRow.append(text)
-                })
-            }
-
+    } else if (col === "action" || col === "popular" || col === "newCol") {
+        collections.parent().removeClass("active");
+        $(this).parent().addClass("active");
+        var colFilter = res.filter(function (el) {
+            return el[col];
         })
+        displayProduct(colFilter);
+    } else {
+        displayProduct(res)
+    }
+
+}
+
+function displayProduct(filter) {
+    var text = "";
+    filter.forEach(function (e) {
+        text = template
+            .replace("{{imgSrc}}", e.imgSrc)
+            .replace(title, e.productTitle)
+            .replace("{{model}}", e.model)
+            .replace("{{price}}", e.price);
+        mainRow.append(text)
+    })
+
 }
